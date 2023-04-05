f718a6e76dbe5ccb41b4ebde78cc0c998f1b1e39
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-04-06 01:24:42 +0530

    Add tests for numbers

---
+++ tests/number.test.js
@@ -0,0 +1,61 @@
+const {
+	getRectangleArea,
+	getAverage,
+	getLinearEquationRoot,
+	getLastDigit,
+	parseNumberFromString,
+	isPrime,
+} = require("../src/numbers");
+
+test(`Testing getRectangleArea`, () => {
+	expect(getRectangleArea(5, 10)).toEqual(50);
+	expect(getRectangleArea(5, 5)).toEqual(25);
+	expect(getRectangleArea(5, 0)).toEqual(0);
+});
+
+test(`Testing getAverage`, () => {
+	expect(getRectangleArea(5, 5)).toEqual(5);
+	expect(getRectangleArea(10, 0)).toEqual(5);
+	expect(getRectangleArea(-5, 5)).toEqual(0);
+	expect(getRectangleArea(6, 5)).toEqual(5.5);
+	expect(getAverage(Number.MAX_VALUE - 2, Number.MAX_VALUE)).toEqual(Number.MAX_VALUE - 1);
+});
+
+test(`Testing getLinearEquationRoot`, () => {
+	expect(getLinearEquationRoot(5, -10)).toEqual(2);
+	expect(getLinearEquationRoot(1, 8)).toEqual(-8);
+	expect(getLinearEquationRoot(5, 0)).toEqual(0);
+});
+
+test(`Testing getLastDigit`, () => {
+	expect(getLastDigit(100)).toEqual(0);
+	expect(getLastDigit(37)).toEqual(7);
+	expect(getLastDigit(5)).toEqual(5);
+	expect(getLastDigit(0)).toEqual(0);
+	expect(getLastDigit(-19)).toEqual(9);
+});
+
+test(`Testing parseNumberFromString`, () => {
+	expect(parseNumberFromString("100")).toEqual(100);
+	expect(parseNumberFromString("37")).toEqual(37);
+	expect(parseNumberFromString("-525.25")).toEqual(-525.25);
+});
+
+test(`Testing isPrime`, () => {
+	expect(isPrime(1)).toBeFalsy();
+	expect(isPrime(4)).toBeFalsy();
+	expect(isPrime(6)).toBeFalsy();
+	expect(isPrime(8)).toBeFalsy();
+	expect(isPrime(9)).toBeFalsy();
+	expect(isPrime(10)).toBeFalsy();
+	expect(isPrime(12)).toBeFalsy();
+	expect(isPrime(119)).toBeFalsy();
+
+	expect(isPrime(2)).toBeTruthy();
+	expect(isPrime(3)).toBeTruthy();
+	expect(isPrime(5)).toBeTruthy();
+	expect(isPrime(7)).toBeTruthy();
+	expect(isPrime(11)).toBeTruthy();
+	expect(isPrime(13)).toBeTruthy();
+	expect(isPrime(113)).toBeTruthy();
+});
