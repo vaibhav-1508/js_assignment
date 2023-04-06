82d95212d71f861211526b1a5a4648eacee9fdd7
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-04-06 15:32:52 +0530

    Add detailed logging in terminal

--- testsScore.js
+++ testsScore.js
@@ -1,19 +1,43 @@
 function getScore(jestObject) {
+	// Initiliaze score trackers
 	let totalScore = 0;
 	let score = 0;
+	const moduleScores = {
+		Arrays: 0,
+		Numbers: 0,
+		Strings: 0,
+		"Conditionals and Loops": 0,
+	};
+	const moduleTotalScores = {
+		Arrays: 0,
+		Strings: 0,
+		"Conditionals and Loops": 0,
+		Numbers: 0,
+	};
 
+	// Populate the results
 	jestObject.testResults.forEach((testResult) => {
 		testResult.testResults.forEach((test) => {
-			const points = parseInt(test.fullName.split(" ")[0].slice(1));
+			// Get the constants
+			const category = test.ancestorTitles[0];
+			const points = parseInt(test.title.split(" ")[0].slice(1));
+
+			// Update totals
 			totalScore += points;
+			moduleTotalScores[category] += points;
+
+			// Update scores if passed
 			if (test.status === "passed") {
 				score += points;
+				moduleScores[category] += points;
 			}
 		});
 	});
 
+	// Log results to terminal
 	console.log("\n\n");
-	console.log(`You scored ${score}/${totalScore} points.`);
+	console.log(`You scored ${score}/${totalScore} points.\n\nHere are your module-wise scores:`);
+	for (mod in moduleScores) console.log(`${mod}\t: ${moduleScores[mod]}/${moduleTotalScores[mod]}`);
 
 	return jestObject;
 }
